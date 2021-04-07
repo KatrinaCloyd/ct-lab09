6fd398d562b621d2f4274d494ec351cb27048227
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-04-06 18:04:03 -0700

    post endpoint test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,8 +11,8 @@ describe('ct-lab09 routes', () => {
   it('post will create a new vacation in the vacations table', async () => {
     const res = await request(app)
       .post('/api/v1/vacations')
-      .send({ destination: 'Hawaii', start: 'May 5th', end: 'May 20th', details: 'going for anniversary celebration' });
-    expect(res.body).toEqual({ id: expect.any(String), destination: 'Hawaii', start: 'May 5th', end: 'May 20th', details: 'going for anniversary celebration' })
+      .send({ destination: 'Hawaii', startDate: 'May 5th', endDate: 'May 20th', details: 'going for anniversary celebration' });
+    expect(res.body).toEqual({ id: expect.any(String), destination: 'Hawaii', startDate: 'May 5th', endDate: 'May 20th', details: 'going for anniversary celebration' });
   });
 
 });
